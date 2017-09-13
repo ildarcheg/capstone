d23ce2475b240450b644c0f2e9a66314db9bfb42
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-09-13 14:07:18 +0700

    updated State controller, State class with time stamp

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,34 +1,26 @@
 class StatesController < ApplicationController
   before_action :set_state, only: [:show, :update, :destroy]
 
-  # GET /states
-  # GET /states.json
   def index
     @states = State.all
-
-    render json: @states
+    #render json: @states
   end
 
-  # GET /states/1
-  # GET /states/1.json
   def show
-    render json: @state
+    #render json: @state
   end
 
-  # POST /states
-  # POST /states.json
   def create
     @state = State.new(state_params)
 
     if @state.save
-      render json: @state, status: :created, location: @state
+      #render json: @state, status: :created, location: @state
+      render :show, status: :created, location: @state
     else
       render json: @state.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /states/1
-  # PATCH/PUT /states/1.json
   def update
     if @state.update(state_params)
       head :no_content
@@ -37,8 +29,6 @@ class StatesController < ApplicationController
     end
   end
 
-  # DELETE /states/1
-  # DELETE /states/1.json
   def destroy
     @state.destroy
 
