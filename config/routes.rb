b0067ea4b6f273f87cf01a3194c85b9c9a07570f
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-09-13 14:10:06 +0700

    routes updated for States api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :states, except: [:new, :edit]
   scope :api, defaults: {format: :json} do 
     resources :cities, except: [:new, :edit]
+    resources :states, except: [:new, :edit]
   end
 end
