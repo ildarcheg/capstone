d545ee30aee6edf65abb0999c38ff278b9e531c2
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-09-13 14:00:06 +0700

    updated City controller, render json changed

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,34 +1,26 @@
 class CitiesController < ApplicationController
   before_action :set_city, only: [:show, :update, :destroy]
 
-  # GET /cities
-  # GET /cities.json
   def index
     @cities = City.all
-
-    render json: @cities
+    #render json: @cities
   end
 
-  # GET /cities/1
-  # GET /cities/1.json
   def show
-    render json: @city
+    #render json: @city
   end
 
-  # POST /cities
-  # POST /cities.json
   def create
     @city = City.new(city_params)
 
     if @city.save
-      render json: @city, status: :created, location: @city
+      #render json: @city, status: :created, location: @city
+      render :show, status: :created, location: @city
     else
       render json: @city.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /cities/1
-  # PATCH/PUT /cities/1.json
   def update
     if @city.update(city_params)
       head :no_content
@@ -37,8 +29,6 @@ class CitiesController < ApplicationController
     end
   end
 
-  # DELETE /cities/1
-  # DELETE /cities/1.json
   def destroy
     @city.destroy
 
