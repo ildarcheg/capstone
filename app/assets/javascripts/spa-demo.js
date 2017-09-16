72b2c6cd13331395f4fac4e4821d1b07d924511c
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-09-16 16:53:04 +0700

    angular added

--- app/assets/javascripts/spa-demo.js
+++ app/assets/javascripts/spa-demo.js
@@ -1,1 +1,8 @@
 // SPA Demo Javascript Manifest File
+
+// SPA Demo Javascript Manifest File
+//= require jquery2
+//= require bootstrap
+//= require angular
+//= require angular-ui-router
+//= require angular-resource
