cc94361d02cba445b7e3c772d05d7b64b4ce72d9
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-09-18 08:20:02 +0700

    changes before adding cities

--- app/assets/javascripts/spa-demo.js
+++ app/assets/javascripts/spa-demo.js
@@ -6,3 +6,7 @@
 //= require angular
 //= require angular-ui-router
 //= require angular-resource
+
+//= require spa-demo/app.module
+//= require spa-demo/app.router
+//= require spa-demo/app.constant
